e4a47db74236d51e6d8bfec40b34c6f93e6439c2
dys9 <dys9@naver.com>
2021-01-21 02:40:39 +0900

    Component Scan

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired // memberService를 스프링 Container에 있는 memberService와 연결
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
